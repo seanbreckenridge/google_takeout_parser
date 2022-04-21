bfe91db0d0b7f75faa31c5fa177a1abeeb5c3cea
Sean Breckenridge <seanbrecke@gmail.com>
2022-04-21 16:21:14 -0700

    use platformdirs to determine cache dir

--- google_takeout_parser/cache.py
+++ google_takeout_parser/cache.py
@@ -2,8 +2,8 @@
 Related to locating the default cache directory for this module
 """
 
-import os
 from pathlib import Path
+from platformdirs import user_cache_dir
 
-cache_dir = os.environ.get("XDG_CACHE_HOME", os.path.join(os.environ["HOME"], ".cache"))
+cache_dir = user_cache_dir()  # handle portability issues/$HOME not being set
 takeout_cache_path = Path(cache_dir) / "google_takeout_parser"
