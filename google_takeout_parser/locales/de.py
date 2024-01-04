b9e47beebffcdb0e47c5af2985acd61a3ce7f9ca
Yann A. Zankl <55953704+parthux1@users.noreply.github.com>
2024-01-04 07:00:58 +0100

    Update de locale (#59)
    * add additional paths for DE locale
    * update folder names
    * skip locale check on windows
    
    ---------
    
    Co-authored-by: Sean Breckenridge <seanbrecke@gmail.com>

--- google_takeout_parser/locales/de.py
+++ google_takeout_parser/locales/de.py
@@ -16,8 +16,9 @@ HANDLER_MAP: HandlerMap = {
     r"Chrome": None,  # Ignore rest of Chrome stuff
     r"Google Play Store/Installs.json": _parse_app_installs,
     r"Google Play Store/": None,  # ignore anything else in Play Store
-    r"Standortverlauf/Semantic Location History/.*/.*.json": _parse_semantic_location_history,
-    r"Standortverlauf/Records.json": _parse_location_history,
+    r"Location History \(Timeline\)/Semantic Location History/.*/.*.json": _parse_semantic_location_history,
+    r"Location History \(Timeline\)/Records.json": _parse_location_history,
+    r"Location History \(Timeline\)/": None,  # ignore anything else in Location History
     # Youtube
     r"YouTube( und YouTube Music)?/Verlauf/.*?.html": _parse_html_activity,
     r"YouTube( und YouTube Music)?/Verlauf/.*?.json": _parse_json_activity,
@@ -31,30 +32,39 @@ HANDLER_MAP: HandlerMap = {
     r"Meine Aktivitäten/.*?Meine\s*Aktivitäten.html": None,
     r"Meine Aktivitäten/.*?Meine\s*Aktivitäten.json": _parse_json_activity,
     # Ignored Google Services
+    r"Google Fit": None,
     r"Google Play-Spieldienste/": None,
     r"Google Developers/": None,
     r"Google Play/": None,
     r"Google Pay": None,
+    r"Google Finanzen/": None,  # stock watchlist
     r"Home App/": None,
     r"Google Shopping": None,
+    r"Google Workspace Marketplace": None,
     r"Google Play Filme _ Serien/": None,
+    r"Google Play Bücher/": None,  # books
     r"Google News/": None,
+    r"Discover/": None,
     r"Google Kontakte/": None,
     r"Gmail/": None,
     r"Google Shopping": None,
     r"Google Unternehmensprofil/": None,
     r"Google Fotos/": None,
     r"Gespeichert/": None,
+    r"Google Chat/": None,
     r"Business Messages/": None,
     r"Classroom/": None,
     r"Google-Konto/": None,
     r"Google-Hilfe-Communities/": None,
     r"Kalender/": None,
-    r"Maps (Meine Orte)/": None,
+    r"Aufgaben/": None,
+    r"Maps \(Meine Orte\)/": None,
     r"Maps/": None,
     r"Profil/": None,
+    r"Groups/": None,
     r"Drive/": None,
     r"Zugriffsprotokollaktivitäten/": None,
+    r"Search Contributions/": None,
     r"Android-Gerätekonfigurationsdienst/": None,
     r"Archiv_Übersicht.html": None,  # description of takeout, not that useful
 }
--- tests/test_locale_paths.py
+++ tests/test_locale_paths.py
@@ -1,14 +1,21 @@
+import os
+
+import pytest
 from google_takeout_parser.locales.main import get_paths_for_functions
 
 
+@pytest.mark.skipif(
+    os.environ.get("RUNNER_OS", "Linux") == "Windows",
+    reason="skipping locale test on windows, some quoting issue",
+)
 def test_locale_paths() -> None:
     jpths = get_paths_for_functions()
     assert jpths == [
         "Chrome",
         "Location History",
+        r"Location History \(Timeline\)",  # using "Location History \\(Timeline\\)" fails on windows.
         "Meine Aktivitäten",
         "My Activity",
-        "Standortverlauf",
         "YouTube( and YouTube Music)?",
         "YouTube( und YouTube Music)?",
     ]
